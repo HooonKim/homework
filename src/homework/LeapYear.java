b65803b715b3f26af37e785e9aa88d09464ef35e
hoon <leamo0809@gmail.com>
2016-04-20 17:46:34 +0900

    과제 완료

--- src/homework/LeapYear.java
+++ src/homework/LeapYear.java
@@ -1,11 +1,30 @@
 package homework;
 
+import java.util.Calendar;
+
 public class LeapYear {
-	public String getEndOfMonth(int year,int month){
-		String day="";
+	public String getEndOfMonth(int year,int month){ 
+		// 코딩하여 완성하시오 
 		
-		// 코딩하여 완성하시오
+		int end_day;
 		
-		return year+"-"+month+"-"+day;
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.set(year, month - 1, 1);
+		
+		
+		if (month < 13) {
+			if (month == 2 && ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0)) {  
+				end_day = 29;  
+			} else {
+				end_day = calendar.getActualMaximum(Calendar.DAY_OF_MONTH); 
+			} 
+			
+			return year+"-"+month+"-"+end_day;
+			
+		} else {
+			
+			return "잘못된 월을 입력하였습니다.";
+		}
 	}
 }
